8e195e10ba4db33234cea38a52ecbe07f2d8d5e0
Nathan Parisod <nathan.parisod@gmail.com>
2026-05-12 12:03:53 +0200

    déploiement

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// IMPORTANT pour GitHub Pages :
-// Remplacer "fissuro-monitor" par le nom EXACT de ton repository GitHub.
-// En local (npm run dev), ça n'a aucune importance.
+
 export default defineConfig({
   plugins: [react()],
-  base: '/fissuro-monitor/',
+  base: '/LTS_proto_site_web/',
 })
